40e78de1d59439b2a4fad41a5684446306ea4bd6
Sandy <syjy8809@gmail.com>
2024-06-19 14:24:41 +0900

    + Define 하던 중

--- Blue_Bunny/Assets/Scripts/Util/Define.cs
+++ Blue_Bunny/Assets/Scripts/Util/Define.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Define : MonoBehaviour
+public class Define
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public const string MONSTER_TAG = "Monster";
 }
