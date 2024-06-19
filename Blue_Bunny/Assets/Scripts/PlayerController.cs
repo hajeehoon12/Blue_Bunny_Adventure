db8202ea94215a55adc068b8182cbd9a0452bc3c
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-19 13:56:30 +0900

    + point filter

--- Blue_Bunny/Assets/Scripts/PlayerController.cs
+++ Blue_Bunny/Assets/Scripts/PlayerController.cs
@@ -4,5 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+
+
+    public float playerSpeed;
+    public float jumpPower;
+
+    private bool canJump;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Update()
+    {
+        JumpCheck();
+    }
+
+
+    private void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+            spriteRenderer.flipX = true;
+        }
+
+        transform.position += moveVelocity * playerSpeed * Time.deltaTime;          
+    }
+
+    private void OnJump()
+    {
+        rigid.AddForce(Vector2.up * jumpPower * rigid.mass, ForceMode2D.Impulse);
+
+    }
+
+    private void JumpCheck()
+    { 
+        
+    }
+
+
+
+
+
 }
