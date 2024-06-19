b2b3707b45bc13492dd64fd821a78b18bc6c9a1d
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-19 21:23:36 +0900

    +PlayerStat

---
+++ Blue_Bunny/Assets/Scripts/Player/PlayerStat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat:MonoBehaviour
+{
+    public float playerSpeed;
+    public float jumpPower;
+    public float bulletSpeed;
+    public float attackDamage;
+}
