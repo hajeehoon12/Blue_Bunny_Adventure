8aa868ec3b725259d1eceb34eb0e8f1ae5817d7e
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-20 18:40:02 +0900

    +Portal image and Camera offset

--- Blue_Bunny/Assets/Scripts/Monster/LightEffect.cs
+++ Blue_Bunny/Assets/Scripts/Monster/LightEffect.cs
@@ -7,7 +7,13 @@ public class LightEffect : MonoBehaviour
 {
 
     Transform target;
+   
 
+
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         transform.position += new Vector3(0, 0, -2);
@@ -18,20 +24,52 @@ public class LightEffect : MonoBehaviour
     void MoveToPlayer()
     {
         target = CharacterManager.Instance.Player.controller.pet.transform;
+        StartCoroutine(Bound());
         StartCoroutine(LifeTime());
+    }
+
+    IEnumerator Bound()
+    {
+        float Dir = CharacterManager.Instance.Player.GetComponent<SpriteRenderer>().flipX ? -1 : 1;
+
+        float time = 0f;
+        float timeInterval = 0.015f;
+
+        while (time < 0.5f)
+        {
+            time += timeInterval;
+            transform.position += new Vector3(Dir * 0.02f, 0.02f);
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+        
+        yield return null;
+        
     }
 
     IEnumerator LifeTime()
     {
 
+        yield return new WaitForSeconds(0.5f);
+        
         float interval = 0.2f;
         float distance = 10f;
         float firstDistance = Vector3.Distance(transform.position, target.position);
+        float time = 0f;
+        float totalTime = 3f;
 
-        while (distance > 0.2f)
+        while (distance > 0.2f || time < 4f)
         {
+            time += 0.2f;
+            float fraction = time / totalTime;
+
+            transform.DOLocalMove(transform.position + (target.position-transform.position) * fraction, interval);
             distance = Vector3.Distance(transform.position, target.position);
-            transform.DOLocalMove(target.position, 4 * distance/firstDistance);
+
+
+
+            //transform.DOLocalMove(target.position, 4 * distance / firstDistance);
+
             yield return new WaitForSeconds(interval);
         }
         Destroy(gameObject);
