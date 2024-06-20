afd48650e03279f97c1149ce56994ce583edb51e
poi001 <107660181+poi001@users.noreply.github.com>
2024-06-21 03:54:15 +0900

    * HPBar, MPBar, ConditionUI

--- Blue_Bunny/Assets/Scripts/UI/HPBar.cs
+++ Blue_Bunny/Assets/Scripts/UI/HPBar.cs
@@ -6,11 +6,23 @@ public class HPBar : SlideBar
 {
     void Start()
     {
-        
+        UpdateBar_Add(0.0f);
     }
 
     void Update()
     {
         
+    }
+
+    //체력이 찰 때, 호출하는 함수
+    public override void UpdateBar_Add(float amount)
+    {
+        base.UpdateBar_Add(amount);
+    }
+
+    //체력이 빠질 때, 호출하는 함수
+    public override void UpdateBar_Sub(float amount)
+    {
+        base.UpdateBar_Sub(amount);
     }
 }
--- Blue_Bunny/Assets/Scripts/UI/SlideBar.cs
+++ Blue_Bunny/Assets/Scripts/UI/SlideBar.cs
@@ -2,32 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public interface ISlideBar
 {
     void UpdateBar_Add(float amount);
     void UpdateBar_Sub(float amount);
     void CheckZeroOrMax();
+    void SetPercent();
 }
 
 public class SlideBar : MonoBehaviour, ISlideBar
 {
     public float Max;
     public float Current;
+    public Image SlideBarImage;
 
     public virtual void UpdateBar_Add(float amount)
     {
         Current += amount;
         CheckZeroOrMax();
+        SetPercent();
     }
     public virtual void UpdateBar_Sub(float amount)
     {
         Current -= amount;
         CheckZeroOrMax();
+        SetPercent();
     }
     public virtual void CheckZeroOrMax()
     {
         Current = Current < 0.0f ? 0.0f : Current;
         Current = Current > Max ? Max : Current;
+    }
+    public virtual void SetPercent()
+    {
+        if (Current <= 0.0f) SlideBarImage.fillAmount = 0.0f;
+        else SlideBarImage.fillAmount = Current / Max;
     }
 }
