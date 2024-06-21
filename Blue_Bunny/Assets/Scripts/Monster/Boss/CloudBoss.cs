61a9b4c8c0bbea564444214aa96047d3cd7ecd9d
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-21 22:41:08 +0900

    *throw summon monster

--- Blue_Bunny/Assets/Scripts/Monster/Boss/CloudBoss.cs
+++ Blue_Bunny/Assets/Scripts/Monster/Boss/CloudBoss.cs
@@ -6,6 +6,7 @@ public class CloudBoss : MonoBehaviour
 {
     public GameObject Bee;
     public GameObject Worm;
+    public GameObject BlueBee;
 
     public LayerMask groundLayerMask;
 
@@ -33,7 +34,7 @@ public class CloudBoss : MonoBehaviour
             switch (patternNum % 2)
             {
                 case 0:
-                    SummonServant();
+                    StartCoroutine(SummonPeriod());
                     break;
 
 
@@ -52,6 +53,16 @@ public class CloudBoss : MonoBehaviour
         
     }
 
+    IEnumerator SummonPeriod()
+    {
+        SummonServant();
+        yield return new WaitForSeconds(1f);
+        SummonServant();
+        yield return new WaitForSeconds(1f);
+        SummonServant();
+
+    }
+
 
     void SummonServant() // Summon Servants
     {
@@ -61,12 +72,10 @@ public class CloudBoss : MonoBehaviour
                 StartCoroutine(Summon(Bee));
                 break;
             case 1:
+                StartCoroutine(Summon(BlueBee));
+                break;
+            default: // temp : safe code
                 StartCoroutine(Summon(Worm));
-                break;
-            case 2:
-                break;
-
-            default:
                 break;
             
         }
@@ -81,8 +90,28 @@ public class CloudBoss : MonoBehaviour
         onFloor = false;
         GameObject servant = Instantiate(mon, transform.position, Quaternion.identity);
         Collider2D servCol = servant.GetComponent<BoxCollider2D>();
-        // 던지기
+        
         Rigidbody2D rigid = servant.GetComponent<Rigidbody2D>();
+
+        servant.GetComponent<Monster>().enabled = false;
+        //rigid.gravityScale = 0f;
+        // 던지기
+        float throwTime = 0;
+        
+
+        float distDiff = 5f;
+        rigid.gravityScale = 0f;
+        while (!(distDiff < 0.5f && distDiff > -0.5f)) // in near distance
+        {
+            float Dir = distDiff > 0 ? -1 : 1;
+            servant.transform.position += new Vector3(0.2f * Dir, 0);
+
+            
+
+            distDiff = (servant.transform.position.x - CharacterManager.Instance.Player.transform.position.x);
+            yield return new WaitForSeconds(0.02f);
+        }
+        rigid.gravityScale = 2f;
 
         while (!onFloor)
         {
@@ -97,11 +126,12 @@ public class CloudBoss : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(servant.transform.position, new Vector2(0, -1), 0.6f, groundLayerMask);
             if (hit.collider?.name != null)
             {
-                servant.transform.position = hit.point + new Vector2(0, servCol.bounds.extents.y + servCol.offset.y);
+                servant.transform.position = hit.point + new Vector2(0, servCol.bounds.extents.y - servCol.offset.y);
                 onFloor = true;
             }
            
         }
+        servant.GetComponent<Monster>().enabled = true;
         rigid.velocity = Vector3.zero;
         Destroy(rigid);
     }
@@ -110,4 +140,8 @@ public class CloudBoss : MonoBehaviour
 
 
 
+    void BossDie()
+    {
+        StopCoroutine(patterCoroutine);
+    }
 }
