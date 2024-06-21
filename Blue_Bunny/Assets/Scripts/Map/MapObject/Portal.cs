4446536763082045e5a8708a76c31e76c5022ab9
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-21 18:17:25 +0900

    +Sound effect update

--- Blue_Bunny/Assets/Scripts/Map/MapObject/Portal.cs
+++ Blue_Bunny/Assets/Scripts/Map/MapObject/Portal.cs
@@ -9,7 +9,9 @@ public class Portal : MonoBehaviour
         if(collision.gameObject.CompareTag(Define.PLAYER_TAG))
         {
             Debug.Log("다음 스테이지로 이동합니다");
+            AudioManager.instance.PlaySFX("Warp", 0.2f);
             GameManager.Instance.ChangeMap();
+            
         }
     }
 }
