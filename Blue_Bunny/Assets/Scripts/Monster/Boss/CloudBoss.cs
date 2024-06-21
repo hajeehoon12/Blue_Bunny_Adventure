a4c8a4b60e5f1638980ede3a62fe14c63807c796
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-21 21:56:07 +0900

    +Worm and bee summon

--- Blue_Bunny/Assets/Scripts/Monster/Boss/CloudBoss.cs
+++ Blue_Bunny/Assets/Scripts/Monster/Boss/CloudBoss.cs
@@ -4,15 +4,110 @@ using UnityEngine;
 
 public class CloudBoss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Bee;
+    public GameObject Worm;
+
+    public LayerMask groundLayerMask;
+
+    Coroutine patterCoroutine;
+
+    private int patternNum = 0;
+    private int monsterNum = 0;
+
+    private bool onFloor = false;
+
+    private void Start()
     {
+        patterCoroutine= StartCoroutine(CloudPattern());
+
+
+
+
+    }
+
+    IEnumerator CloudPattern()
+    {
+
+        while (true)
+        {
+            switch (patternNum % 2)
+            {
+                case 0:
+                    SummonServant();
+                    break;
+
+
+
+
+                default:
+                    break;
+            
+            }
+
+            yield return new WaitForSeconds(3f);
+
+            patternNum++;
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void SummonServant() // Summon Servants
     {
-        
+        switch (monsterNum % 3)
+        {
+            case 0:
+                StartCoroutine(Summon(Bee));
+                break;
+            case 1:
+                StartCoroutine(Summon(Worm));
+                break;
+            case 2:
+                break;
+
+            default:
+                break;
+            
+        }
+
+        monsterNum++;
     }
+    
+    IEnumerator Summon(GameObject mon)
+    {
+
+        
+        onFloor = false;
+        GameObject servant = Instantiate(mon, transform.position, Quaternion.identity);
+        Collider2D servCol = servant.GetComponent<BoxCollider2D>();
+        // 던지기
+        Rigidbody2D rigid = servant.GetComponent<Rigidbody2D>();
+
+        while (!onFloor)
+        {
+            yield return new WaitForSeconds(0.01f);
+
+            if (servant.transform.position.y - CharacterManager.Instance.Player.transform.position.y >= 1f)
+            {
+                continue;
+            }
+
+
+            RaycastHit2D hit = Physics2D.Raycast(servant.transform.position, new Vector2(0, -1), 0.6f, groundLayerMask);
+            if (hit.collider?.name != null)
+            {
+                servant.transform.position = hit.point + new Vector2(0, servCol.bounds.extents.y + servCol.offset.y);
+                onFloor = true;
+            }
+           
+        }
+        rigid.velocity = Vector3.zero;
+        Destroy(rigid);
+    }
+
+
+
+
+
 }
