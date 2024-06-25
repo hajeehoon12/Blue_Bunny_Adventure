cf3060e81834e3f6d6dd37173cba4b88caf6ca34
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-06-25 10:35:25 +0900

    +Gold Amount UI

---
+++ Blue_Bunny/Assets/Scripts/UI/PlayerGold.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerGold : MonoBehaviour
+{
+    public Text goldText;
+
+    private void Update()
+    {
+        goldText.text = CharacterManager.Instance.Player.stats.playerGold.ToString();
+    }
+
+}
